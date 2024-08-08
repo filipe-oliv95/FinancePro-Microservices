24e301b63d205e17a557cccb43b8eece649a2122
Filipe <filipec.oliveira95@gmail.com>
2024-08-08 18:55:06 -0300

    feat: Build empty Spring Boot application
    - Set up a new Spring Boot project using Java 17 and Maven.
    - Added dependencies: Spring Web, Actuator, JPA, DevTools, Validations, H2 Database, Lombok.

---
+++ accounts/src/test/java/com/financepro/accounts/AccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financepro.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
