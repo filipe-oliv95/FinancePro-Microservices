4336321c3eba6be167954e2f4690890364a69982
Filipe <filipec.oliveira95@gmail.com>
2024-08-08 19:56:04 -0300

    feat: Create entities and DTOs for Accounts, Customer, and Response
    - Created Accounts entity and AccountsDto.
    - Created Customer entity and CustomerDto.
    - Added a base class BaseEntity for common entity properties.
    - Developed ResponseDto for standard API responses.
    - Developed ErrorResponseDto for error handling responses.

---
+++ accounts/src/main/java/com/financepro/accounts/entity/Accounts.java
@@ -0,0 +1,24 @@
+package com.financepro.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class Accounts extends BaseEntity{
+
+    @Column(name="customer_id")
+    private Long customerId;
+
+    @Id
+    @Column(name="account_number")
+    private Long accountNumber;
+
+    @Column(name="account_type")
+    private String accountType;
+
+    @Column(name="branch_adress")
+    private String branchAddress;
+}
