840573a34a23f6ca4e9ff4593018f8dd5880b9d0
Filipe <filipec.oliveira95@gmail.com>
2024-08-08 22:08:56 -0300

    feat: Create mapper logic: DTO -> entity and entity -> DTO
    - Implemented mapping logic to convert between DTOs and entities and vice versa.
    - Ensured accurate data transfer between different layers of the application.

---
+++ accounts/src/main/java/com/financepro/accounts/mapper/AccountsMapper.java
@@ -0,0 +1,22 @@
+package com.financepro.accounts.mapper;
+
+import com.financepro.accounts.dto.AccountsDto;
+import com.financepro.accounts.entity.Accounts;
+
+public class AccountsMapper {
+
+    public static AccountsDto mapToAccountsDto(Accounts accounts, AccountsDto accountsDto) {
+        accountsDto.setAccountNumber(accounts.getAccountNumber());
+        accountsDto.setAccountType(accounts.getAccountType());
+        accountsDto.setBranchAddress(accounts.getBranchAddress());
+        return accountsDto;
+    }
+
+    public static Accounts mapToAccounts(AccountsDto accountsDto, Accounts accounts) {
+        accounts.setAccountNumber(accountsDto.getAccountNumber());
+        accounts.setAccountType(accountsDto.getAccountType());
+        accounts.setBranchAddress(accountsDto.getBranchAddress());
+        return accounts;
+    }
+
+}
