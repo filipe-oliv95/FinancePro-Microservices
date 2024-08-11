47e4d4bcfd6dbcb1322a393e5501aaa941bee41e
Filipe <filipec.oliveira95@gmail.com>
2024-08-11 18:26:28 -0300

    feat: Add @OpenAPIDefinition annotation
    Added @OpenAPIDefinition annotation to the main application class to configure OpenAPI documentation settings.

--- accounts/src/main/java/com/financepro/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/financepro/accounts/AccountsApplication.java
@@ -1,11 +1,36 @@
 package com.financepro.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "FinancePro Accounts microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Filipe Oliveira",
+						email = "filipec.oliveira95@gmail.com",
+						url = "https://github.com/filipe-oliv95"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/filipe-oliv95/FinancePro-Microservices"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description =  "FinancePro Accounts microservice REST API Documentation",
+				url = "https://github.com/filipe-oliv95/FinancePro-Microservices"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
