0662ae3f2a1c12b74f97ac11776c9d8c8e2fa5b4
Filipe <filipec.oliveira95@gmail.com>
2024-08-11 19:08:57 -0300

    feat: Add documentation information in AccountsController
    - Added @Tag for grouping and describing controller.
    - Added @Operation for describing API operations.
    - Added @ApiResponses for detailing possible API responses.
    - Added @Parameter to improve parameter descriptions.

--- accounts/src/main/java/com/financepro/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/financepro/accounts/controller/AccountsController.java
@@ -1,11 +1,17 @@
 package com.financepro.accounts.controller;
 
 import com.financepro.accounts.constants.AccountsConstants;
-import com.financepro.accounts.dto.AccountsDto;
 import com.financepro.accounts.dto.CustomerDto;
+import com.financepro.accounts.dto.ErrorResponseDto;
 import com.financepro.accounts.dto.ResponseDto;
-import com.financepro.accounts.service.impl.AccountsServiceImpl;
 import com.financepro.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -14,6 +20,20 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "CRUD REST APIs for Accounts in FinancePro",
+        description = "CRUD REST APIs in FinancePro to CREATE, UPDATE, FETCH AND DELETE account details"
+)
+@ApiResponses({
+        @ApiResponse(
+                responseCode = "500",
+                description = "HTTP Status Internal Server Error",
+                content = @Content(
+                        mediaType = "application/json",
+                        schema = @Schema(implementation = ErrorResponseDto.class)
+                )
+        )
+})
 @RestController
 @RequestMapping(path = "/api")
 @AllArgsConstructor
@@ -22,7 +42,22 @@ public class AccountsController {
 
     private IAccountsService iAccountsService;
 
-    @PostMapping("/create")
+    @Operation(
+            summary = "Create Account REST API",
+            description = "REST API to create new Customer & Account inside FinancePro"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "HTTP Status CREATED",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDto.class)
+                    )
+            )
+    }
+    )
+    @PostMapping(value = "/create", produces = "application/json")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         iAccountsService.createAccount(customerDto);
         return ResponseEntity
@@ -30,8 +65,32 @@ public class AccountsController {
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
-    @GetMapping("/fetch")
+    @Operation(
+            summary = "Fetch Account Details REST API",
+            description = "REST API to fetch Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = CustomerDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP Status Not Found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            )
+    }
+    )
+    @GetMapping(value = "/fetch", produces = "application/json")
     public ResponseEntity<CustomerDto> fetchAccount (@RequestParam
+                                                         @Parameter(description = "Mobile number with exactly 10 digits ", example = "7289046712", required = true)
                                                          @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                          String mobileNumber) {
         CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
@@ -40,7 +99,38 @@ public class AccountsController {
                 .body(customerDto);
     }
 
-    @PutMapping("/update")
+    @Operation(
+            summary = "Update Account Details REST API",
+            description = "REST API to update Customer &  Account details based on a account number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP Status Not Found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDto.class)
+                    )
+            )
+    }
+    )
+    @PutMapping(value = "/update", produces = "application/json")
     public ResponseEntity<ResponseDto> updateAccount(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = iAccountsService.updateAccount(customerDto);
         if (isUpdated) {
@@ -54,8 +144,40 @@ public class AccountsController {
         }
     }
 
-    @DeleteMapping("/delete")
+    @Operation(
+            summary = "Delete Account & Customer Details REST API",
+            description = "REST API to delete Customer &  Account details based on a mobile number"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP Status OK",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "HTTP Status Not Found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ErrorResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "417",
+                    description = "Expectation Failed",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseDto.class)
+                    )
+            )
+    }
+    )
+    @DeleteMapping(value = "/delete", produces = "application/json")
     public ResponseEntity<ResponseDto> deleteAccount(@RequestParam
+                                                         @Parameter(description = "Mobile number with exactly 10 digits ", example = "7289046712", required = true)
                                                          @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                          String mobileNumber) {
         boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
